40744308f2108d47c6ca27a112f650242bf25fda
jamespagedev <jamespagedev@outlook.com>
2019-01-18 09:46:58 -0700

    migrations and database added

---
+++ migrations/20190118092418_create_projects_table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', function(tbl) {
+    // Primary Key 'id'
+    tbl.increments();
+
+    // Other Columns
+    tbl
+      .string('name', 128)
+      .notNullable()
+      .unique();
+    tbl.text('description').notNullable();
+    tbl.boolean('completed').defaultTo(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('projects');
+};
